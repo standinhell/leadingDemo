9c1a8e92446db300df4d0e578ccb07595fc37005
zhaoshenqiao <zsqqfyun@163.com>
2018-10-11 18:25:04 +0800

    20181011

--- orders/src/main/java/com/leading/orders/OrdersApplication.java
+++ orders/src/main/java/com/leading/orders/OrdersApplication.java
@@ -1,10 +1,17 @@
 package com.leading.orders;
 
+import io.shardingsphere.shardingjdbc.api.MasterSlaveDataSourceFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.ResourceUtils;
+
+import javax.sql.DataSource;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -15,4 +22,5 @@ public class OrdersApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrdersApplication.class, args);
 	}
+
 }
