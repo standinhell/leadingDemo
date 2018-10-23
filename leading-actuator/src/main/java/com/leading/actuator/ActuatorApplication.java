b519ce05dfcc44f00ef96ee9303751629323ddb3
zhaoshenqiao <zsqqfyun@163.com>
2018-10-23 16:34:21 +0800

    actuator init

---
+++ leading-actuator/src/main/java/com/leading/actuator/ActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.leading.actuator;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class ActuatorApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ActuatorApplication.class,args);
+    }
+}
--- product/src/main/java/com/leading/product/config/DruidConfig.java
+++ product/src/main/java/com/leading/product/config/DruidConfig.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-@Configuration
+//@Configuration
 public class DruidConfig {
     @ConfigurationProperties(prefix = "spring.datasource")
     @Bean
